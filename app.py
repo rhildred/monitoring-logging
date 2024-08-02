4bc81a49f3211aed26f8c382679c3d03d8344329
Richard Hildred <rhildred@gmail.com>
2024-08-02 16:45:54 +0000

    soak test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -14,14 +14,18 @@ Base.prepare(autoload_with=engine)
 # mapped classes are now created with names by default
 # matching that of the table name.
 Product = Base.classes.products
+session = Session(engine)
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
 @app.route('/products/<id>')
 def products(id):
-    session = Session(engine)
     u1 = session.query(Product).filter(Product.id == id).first()
     return jsonable_encoder(u1), 200
